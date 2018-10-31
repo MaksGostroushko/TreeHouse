fcef5843344276f989472c03c3576158380b2395
MaksGostroushko <32840948+MaksGostroushko@users.noreply.github.com>
2018-10-31 21:11:21 +0200

    Create contact_list.(We can add but can't display)
    This is a real phone book containing contacts and mobile phones

---
+++ contact_list.rb
@@ -0,0 +1,30 @@
+def ask(question, kind="string")
+  print question + " "
+  answer = gets.chomp
+  answer = answer.to_i if kind == "number"
+  return answer
+end
+
+def add_contact
+  contact = {"name" => "", "phone_numbers" => []}
+  contact["name"] = ask("What is the person's name?")
+  answer = ""
+  while answer != "n"
+    answer = ask("Do you want add a phone number? (y/n)")
+    if answer == "y"
+      phone = ask("Enter a phone nuber:")
+      contact["phone_numbers"].push(phone)
+    end
+  end
+  return contact
+end
+
+contact_list = []
+
+answer = ""
+while  answer != "n"
+  contact_list.push(add_contact())
+  answer = ask("Add another? (y/n)")
+end
+
+puts "---"
